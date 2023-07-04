dee5a5e8953d6c16147e945b9e2ddf393417c5a7
johnie261 <njorogejohn261@gmail.com>
2023-07-04 16:11:58 +0300

    Modify routes

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,8 @@ class Category < ApplicationRecord
     belongs_to :user
     has_many :expenses, dependent: :destroy
 
+    has_one_attached :icon
+
     validates :name, presence: true
     validates :icon, presence: true
 
